50cc097abc3292b6af4f3d27bf8bcdac937215f7
Patrick Liang <pl204511194@yahoo.com>
2022-04-12 11:36:51 -0400

    commit tree c96ed95aa3f75f60e834c38b89c0a3d49cd85b12

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,11 +1,25 @@
 function findShortestString(arr) {
   // type your code here
+
+  for (let i = 0; i < arr.length; i++) {
+    let num = i
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j].length < arr[num].length) {
+        num = j
+      }
+    } if (num != i) {
+      let temp = arr[i]
+      arr[i] = arr[num]
+      arr[num] = temp
+    }
+  }
+  return arr[0]
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'a'");
-  console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
+  console.log("=>", findShortestString(['aaa']));
 
   console.log("");
 
